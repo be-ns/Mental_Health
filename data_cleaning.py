ee4a3b38f67f6381f3b50215a52ec4f78a64f48d
be-ns <benjaminsiverly@gmail.com>
2017-07-10 17:31:06 -0600

    no comment

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,26 +2,26 @@ import pandas as pd
 import numpy as np
 
 
-def gender_category(data):
+def _gender_category(data):
     male = ['male', 'm', 'mal','make', 'man', 'guy', 'mail', 'male-ish', 'maile', 'male (cis)', 'cis male']
     female = ['female', 'femake', 'femail', 'cis-female/femme', 'female ', 'woman', 'girl']
     data.Gender = data.Gender.apply(lambda x: 1 if x.lower() in male else 2 if x.lower() in female else 3)
     return data
 
 
-def drop_columns(data):
+def _drop_columns(data):
     data.drop_columns(['comments', 'Timestamp', 'state', 'self_employed'])
 
 
-def tech(data):
+def _tech(data):
     data.tech_company = data.tech_company.apply(lambda x: 1 if x == 'Yes' else 0)
-    print(type(data))
     return data
 
 
 def clean_data(data):
-    data = gender_category(data)
-    data = tech(data)
+    data = _gender_category(data)
+    data = _tech(data)
+    data = _drop_(data)
     return data
 
 if __name__ == '__main__':
