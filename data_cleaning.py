fd6cb9c96ae998f894b414ca984abae4017439e0
be-ns <benjaminsiverly@gmail.com>
2017-07-10 17:02:20 -0600

    added tech sector

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,7 +8,15 @@ def gender_category(data):
     data.Gender = data.Gender.apply(lambda x: 1 if x.lower() in male else 2 if x.lower() in female else 3)
     return data
 
+def tech(data):
+    data.tech_company = data.tech_company.apply(lambda x: 1 if x == 'Yes' else 0)
+    return data
+
+
+def clean_data(data):
+    data = tech(gender_category(data))
+
 if __name__ == '__main__':
     data = pd.read_csv('survey.csv')
-    data = gender_category(data)
-    print(data.Gender.head(12))
+    data = clean_data(data)
+    print(data.head(10))
