b54fc89466b0affa688cdb31f3facd784f9ea945
be-ns <benjaminsiverly@gmail.com>
2017-07-10 15:51:08 -0600

    fresh

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,9 @@ import numpy as np
 def gender_category(data):
     male = ['male', 'm', 'mal','make', 'man', 'guy', 'mail', 'male-ish', 'maile', 'male (cis)', 'cis male']
     female = ['female', 'femake', 'femail', 'cis-female/femme', 'female ', 'woman', 'girl']
+    data.Gender = data.Gender.apply(lambda x: 1 if x in male else 2 if x in female else 3)
+    return data
 
 if __name__ == '__main__':
     data = pd.read_csv('survey.csv')
-    print(data.head(10))
+    data = gender_category(data)
