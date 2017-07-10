e8e53de1315922b152d33a923b86ed935239c98f
be-ns <benjaminsiverly@gmail.com>
2017-07-10 15:50:22 -0600

    gender transpose added

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,11 @@
 import pandas as pd
 import numpy as np
 
+
+def gender_category(data):
+    male = ['male', 'm', 'mal','make', 'man', 'guy', 'mail', 'male-ish', 'maile', 'male (cis)', 'cis male']
+    female = ['female', 'femake', 'femail', 'cis-female/femme', 'female ', 'woman', 'girl']
+
 if __name__ == '__main__':
     data = pd.read_csv('survey.csv')
     print(data.head(10))
