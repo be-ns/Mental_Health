92736dfe320e18f8c6259f0682e332f291a1db6f
be-ns <benjaminsiverly@gmail.com>
2017-07-10 15:53:34 -0600

    .lower()

--- data_cleaning.py
+++ data_cleaning.py
@@ -5,7 +5,7 @@ import numpy as np
 def gender_category(data):
     male = ['male', 'm', 'mal','make', 'man', 'guy', 'mail', 'male-ish', 'maile', 'male (cis)', 'cis male']
     female = ['female', 'femake', 'femail', 'cis-female/femme', 'female ', 'woman', 'girl']
-    data.Gender = data.Gender.apply(lambda x: 1 if x in male else 2 if x in female else 3)
+    data.Gender = data.Gender.apply(lambda x: 1 if x.lower() in male else 2 if x.lower() in female else 3)
     return data
 
 if __name__ == '__main__':
