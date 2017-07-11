f426b23ceb4e77405aa6bb2dee1e9969fe5b6f23
be-ns <benjaminsiverly@gmail.com>
2017-07-10 18:59:00 -0600

    edit

--- data_cleaning.py
+++ data_cleaning.py
@@ -10,7 +10,7 @@ def _gender_category(data):
 
 
 def _drop_(data):
-    data.drop(labels=['comments', 'Timestamp', 'state', 'self_employed'], axis = 1, inplace = True)
+    data.drop(labels=['coworkers', 'comments', 'Timestamp', 'state', 'self_employed', 'benefits', ], axis = 1, inplace = True)
     return data
 
 
@@ -19,11 +19,51 @@ def _tech(data):
     return data
 
 
+def _check_for_socialized_healthcare(data):
+    socialized = {'United States': 68.35, 'Canada':69.77, 'United Kingdom': 74.26, /
+    'Bulgaria': 52.72, 'France': 78.40, 'Portugal': 69.73, /
+    'Netherlands': 82.22, 'Switzerland': 71.74, 'Poland': 62.33, /
+    'Australia': 75.42, 'Germany': 75.99, 'Russia': 56.13, /
+    'Mexico': 69.82, 'Brazil': 52.44, 'Slovenia': 65.15, /
+    'Costa Rica': 0.0, 'Austria': 79.47, 'Ireland': 53.12, /
+    'India': 67.50, 'South Africa': 62.55, 'Italy': 66.69, /
+    'Sweden': 70.56, 'Colombia': 66.79, 'Latvia': 0.0, /
+    'Romania': 53.84, 'Belgium': 79.85, 'New Zealand': 71.65, /
+    'Zimbabwe': 0.0, 'Spain': 76.34, 'Finland': 74.61, /
+    'Uruguay': 0.0, 'Israel': 74.98, 'Bosnia and Herzegovina': 60.57, /
+    'Hungary': 52.29, 'Singapore': 68.50, 'Japan': 81.33, /
+    'Nigeria': 0.0, 'Croatia': 65.05, 'Norway': 74.96, /
+    'Thailand': 80.57, 'Denmark': 76.17, 'Bahamas, The': 0.0, /
+    'Greece': 54.64, 'Moldova': 0.0, 'Georgia': 0.0, /
+    'China': 61.56,'Czech Republic': 75.48, 'Philippines': 68.46}
+    data.Country = data.Country.apply(lambda x: socialized[x] if x in socialized.values() else 0.0)
+    return data
+
+
+def _treatment(data):
+    data.treatment = data.treatment.apply(lambda x: 1 if x == 'Yes' else 0)
+    return data
+
+
+def _remote_work(data):
+    data.remote_work = data.remote_work.apply(lambda x: 1 if x == 'Yes' else 0)
+    return data
+
+
 def clean_data(data):
     data = _gender_category(data)
     data = _tech(data)
     data = _drop_(data)
+    data = _check_for_socialized_healthcare(data)
+    data = _treatment(data)
+    data = _remote_work(data)
     return data
+
+
+def supervisor(data):
+    data.supervisor = data.supervisor.apply(lambda x: 1 if x == 'Yes' else 0 if x == 'No' else 2)
+    return data
+
 
 if __name__ == '__main__':
     data = pd.read_csv('survey.csv')
