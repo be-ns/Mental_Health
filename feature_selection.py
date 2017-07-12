6fa46388098d4dc421a2ca4c2a34a48168425ca3
be-ns <benjaminsiverly@gmail.com>
2017-07-12 14:48:10 -0600

    starting lasso

--- feature_selection.py
+++ feature_selection.py
@@ -3,9 +3,13 @@ from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import f1_score as f1
 from data_cleaning import clean_data as cd
 import pandas as pd
+from sklearn.linear_model import Lasso as lass_o
 
 
 def _train_test(X, y):
+    y = data.treatment
+    data.drop(['treatment'], axis = 1, inplace = True)
+    X = data
     x_tr, x_te, y_tr, y_te = tts(X, y)
     return x_tr, x_te, y_tr, y_te
 
@@ -13,10 +17,8 @@ def _score_model(pred, true):
     print(f1(true, pred))
 
 def build_model(data):
-    y = data.treatment
-    data.drop(['treatment'], axis = 1, inplace = True)
-    X = data
-    x_tr, x_te, y_tr, y_te = _train_test(X, y)
+    x_tr, x_te, y_tr, y_te = _train_test(data)
+    print('starting Random Forest')
     forest = rfc(n_estimators=10000,n_jobs=-1, verbose=1, class_weight="balanced")
     forest.fit(x_tr, y_tr)
     predicted = forest.predict(x_te)
@@ -24,6 +26,7 @@ def build_model(data):
     zipped = zip(data.columns.tolist(), forest.feature_importances_)
     for x in list(sorted(zipped, key=lambda x: x[1])):
         print(x)
+
 
 
 if __name__ == '__main__':
