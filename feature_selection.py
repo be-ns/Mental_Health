5547ce3c400c43fee49800d9aca3132ba06ec740
be-ns <benjaminsiverly@gmail.com>
2017-07-12 19:52:00 -0600

    added extra functions

--- feature_selection.py
+++ feature_selection.py
@@ -22,29 +22,36 @@ def _train_test(data, preprocess):
 def _score_model(pred, true):
     print(f1(true, pred))
 
-def build_model(data):
-    x_tr, x_te, y_tr, y_te = _train_test(data, preprocess = 0)
-    print('starting Random Forest')
-    forest = rfc(n_estimators=10000,n_jobs=-1, verbose=1, class_weight="balanced")
+def _build_r_forest(x_tr, x_te, y_tr, y_te):
+    forest = rfc(n_estimators=1000, n_jobs=-1, verbose=1, class_weight="balanced")
     forest.fit(x_tr, y_tr)
     predicted = forest.predict(x_te)
     _score_model(predicted, y_te)
     zipped = zip(data.columns.tolist(), forest.feature_importances_)
     for x in list(sorted(zipped, key=lambda x: x[1])):
         print(x)
-    print('starting KNN')
-    x_tr, x_te, y_tr, y_te = _train_test(cd(pd.read_csv('survey.csv')), preprocess = 1)
+
+def _build_knn(x_tr, x_te, y_tr, y_te):
     knn = kn_n(n_neighbors = 3)
     knn.fit(x_tr, y_tr)
     predicted = knn.predict(x_te)
     _score_model(predicted, y_te)
-    print('starting SVM')
-    x_tr, x_te, y_tr, y_te = _train_test(cd(pd.read_csv('survey.csv')), preprocess=0)
-    svm = sv_c(C=.75)
+
+def _build_svm(x_tr, x_te, y_tr, y_te):
+    svm = sv_c(C=1, kernel = 'linear')
     svm.fit(x_tr, y_tr)
     predicted = svm.predict(x_te)
     _score_model(predicted, y_te)
 
 
+def build_model(data):
+    print('starting Random Forest')
+    _build_r_forest(_train_test(data, preprocess = 0))
+    print('starting KNN')
+    _build_knn(_train_test(cd(pd.read_csv('survey.csv')), preprocess = 1))
+    print('starting SVM')
+    _build_svm(_train_test(cd(pd.read_csv('survey.csv')), preprocess=1))
+
+
 if __name__ == '__main__':
-    build_model(cd(pd.read_csv('survey.csv')))
+    build_model(cd(pd.read_csv ('survey.csv')))
